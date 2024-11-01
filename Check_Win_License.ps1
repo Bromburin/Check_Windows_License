7692e342b2b5b6aabd5c34535b55cbad76f3292a
Brom <58997615+Bromburin@users.noreply.github.com>
2024-11-01 19:20:52 +0100

    Update Check_Win_License.ps1

--- Check_Win_License.ps1
+++ Check_Win_License.ps1
@@ -2,23 +2,28 @@
 ## v 1.0
 ##
 ## by Brom
-## 
-$licencestatus = 0
-$ProductKey = 0
-$License = 0 
 
-$licencestatus = (Get-CimInstance -ClassName SoftwareLicensingProduct | where {$_.name -match "windows" -and $_.PartialProductKey}  | select LicenseStatus)
-$License = (Get-CimInstance -ClassName SoftwareLicensingService).OA3xOriginalProductKey
-$ProductKey = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform" -Name BackupProductKeyDefault).BackupProductKeyDefault
+# Initialize variables
+$licenseStatus = 0
+$productKey = ""
+$uefiKey = ""
 
-		switch ($licencestatus) {
-		"@{LicenseStatus=1}" {write-host "Windows license `n Registry Windows Key: $ProductKey `n UEFI Windows Key: $License";break}
-		"@{LicenseStatus=0}" {write-host "!!Windows not licensed!!";break}
-		"@{LicenseStatus=2}" {write-host "Out-Of-Box Grace Period";break}
-		"@{LicenseStatus=3}" {write-host "Out-Of-Tolerance Grace Period";break}
-		"@{LicenseStatus=4}" {write-host "Non-Genuine Grace Period";break}
-		"@{LicenseStatus=5}" {write-host "Notification";break}
-		"@{LicenseStatus=6}" {write-host "Extended Grace";break}
-		default {write-host = "value not found"}
-		}
-exit 
+# Retrieve license status and keys
+$licenseStatus = (Get-CimInstance -ClassName SoftwareLicensingProduct | Where-Object { $_.Name -match "Windows" -and $_.PartialProductKey } | Select-Object -ExpandProperty LicenseStatus)
+$uefiKey = (Get-CimInstance -ClassName SoftwareLicensingService).OA3xOriginalProductKey
+$productKey = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform" -Name BackupProductKeyDefault).BackupProductKeyDefault
+
+# Display license status
+switch ($licenseStatus) {
+	1 { Write-Host "Windows license valid`nRegistry Windows Key: $productKey`nUEFI Windows Key: $uefiKey" }
+	0 { Write-Host "!! Windows not licensed !!" }
+	2 { Write-Host "Out-Of-Box Grace Period" }
+	3 { Write-Host "Out-Of-Tolerance Grace Period" }
+	4 { Write-Host "Non-Genuine Grace Period" }
+	5 { Write-Host "Notification" }
+	6 { Write-Host "Extended Grace" }
+	default { Write-Host "Value not found" }
+}
+
+# Exit the script
+exit
